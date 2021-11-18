360b23ba3e84e1f9a0fa01dd091de1a3701bdc56
Aaron Li <aaronhf.li@mail.utoronto.ca>
2021-11-18 14:45:49 -0500

    Add in allowed host

--- HextomTakeHome/settings.py
+++ HextomTakeHome/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['rocky-wildwood-70316.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['rocky-wildwood-70316.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
